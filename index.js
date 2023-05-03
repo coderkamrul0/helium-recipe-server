b77038c860b53e62d67fe9d77d700c453b78685f
Kamrul Hasan <kamrul444216@gmail.com>
2023-05-03 21:41:22 +0600

    Server side setup

---
+++ index.js
@@ -0,0 +1,17 @@
+const express = require('express');
+const app = express();
+const cors = require('cors');
+const port = process.env.PORT || 5000;
+
+const chef = require('./data/chef.json');
+
+app.use(cors());
+
+
+app.get('/', (req, res) =>{
+    res.send(chef);
+})
+
+app.listen(port, () => {
+    console.log(`API is running on port: ${port}`)
+})
