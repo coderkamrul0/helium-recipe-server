d6a9c08eb182dbb0315da7bfc1989fb8544eccca
Kamrul Hasan <kamrul444216@gmail.com>
2023-05-03 21:47:47 +0600

    server side lunch succesfully done

--- index.js
+++ index.js
@@ -2,7 +2,6 @@ const express = require('express');
 const app = express();
 const cors = require('cors');
 const port = process.env.PORT || 5000;
-
 const chef = require('./data/chef.json');
 
 app.use(cors());
